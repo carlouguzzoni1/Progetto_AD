79403b46a42ceca033bf3e27f4d77ed991d68d2e
carlouguzzoni1 <carlouguzzoni@gmail.com>
2024-12-26 12:11:56 +0100

    refactor clients + auth/del/login/logout

--- NameServer.py
+++ NameServer.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sqlite3
 import rpyc
 from bcrypt import hashpw, gensalt, checkpw
@@ -17,12 +18,11 @@ class NameServerService(rpyc.Service):
     To-do: finire documentazione.
     """
 
-    def __init__(self):
+    def __init__(self, host="localhost", port=SERVER_PORT):
         self.db_path        = DB_PATH
-        self.server_port    = SERVER_PORT
+        self.server_port    = port
         self._setup_database()
-        # To-do: inserire procedura di inizializzazione dei file servers
-        # presenti nel database.
+        # To-do: inserire procedura di inizializzazione dei file servers presenti nel database.
 
 
     def _setup_database(self):
@@ -36,18 +36,19 @@ class NameServerService(rpyc.Service):
             # Create the database.
             conn = sqlite3.connect(self.db_path)
             cursor = conn.cursor()
-            # Users table.
+            
+            # Create users table.
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS users (
                     username TEXT PRIMARY KEY,
                     password_hash TEXT NOT NULL,
-                    is_root BOOLEAN DEFAULT 0
+                    is_root BOOLEAN DEFAULT 0,
+                    is_online BOOLEAN DEFAULT 0,
+                    directory TEXT
                 );
             """)
             
-            # To-do: aggiungere le tabelle relative a:
-            # - File servers
-            # - File e metadati
+            # To-do: aggiungere le altre tabelle - file servers/metadati?
             
             conn.commit()
             conn.close()
@@ -61,20 +62,27 @@ class NameServerService(rpyc.Service):
             username (str): The username of the new user.
             password (str): The password of the new user.
             is_root (bool): Whether the new user is a root user.
+        Returns:
+            str:            A message indicating the result of the operation.
         """
+        
         conn            = sqlite3.connect(self.db_path)
         cursor          = conn.cursor()
         hashed_password = hashpw(password.encode('utf-8'), gensalt())
-
+        directory       = "./CLI/" + username
+        
         try:
+            # Create the user.
             cursor.execute(
                 """
-                INSERT INTO users (username, password_hash, is_root)
-                VALUES (?, ?, ?)
+                INSERT INTO users (username, password_hash, is_root, is_online, directory)
+                VALUES (?, ?, ?, ?, ?)
                 """,
-                (username, hashed_password, is_root)
+                (username, hashed_password, is_root, False, directory)
             )
             conn.commit()
+            # Create the directory for the user.
+            os.mkdir(directory)
             return f"User '{username}' created successfully."
         except sqlite3.IntegrityError:
             return f"Error: user '{username}' already exists."
@@ -82,14 +90,85 @@ class NameServerService(rpyc.Service):
             conn.close()
 
 
-"""
-    def exposed_list_users(self):
-        conn = sqlite3.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT username, is_root FROM users")
-        users = cursor.fetchall()
+    def exposed_authenticate(self, username, password):
+        """
+        Authenticates a user.
+        Args:
+            username (str): The username of the user.
+            password (str): The password of the user.
+        Returns:
+            dict:           A dictionary containing the result of the operation.
+        """
+        
+        # To-do: aggiungere controllo on-line/off-line?
+        conn    = sqlite3.connect(self.db_path)
+        cursor  = conn.cursor()
+        cursor.execute("SELECT password_hash FROM users WHERE username = ?", (username,))
+        result  = cursor.fetchone()
+        
+        report = dict()
+        
+        if result is None:
+            report["status"]    = False
+            report["message"]   = f"Error: user '{username}' not found."
+            return report
+        
+        # Check password.
+        password_match = checkpw(password.encode('utf-8'), result[0])
+        
+        # Get user directory.
+        cursor.execute("SELECT directory FROM users WHERE username = ?", (username,))
+        directory = cursor.fetchone()[0]
+        
+        if password_match:
+            report["status"]    = True
+            report["message"]   = f"User '{username}' authenticated successfully."
+            report["directory"] = directory
+        else:
+            report["status"]    = False
+            report["message"]   = f"Error: wrong password for user '{username}'."
+        
         conn.close()
-        return [{"username": u[0], "is_root": bool(u[1])} for u in users] """
+        
+        return report
+
+
+    def exposed_delete_user(self, username, password):
+        """
+        Deletes a user.
+        Args:
+            username (str): The username of the user.
+            password (str): The password of the user.
+        Returns:
+            str:            A message indicating the result of the operation.
+        """
+        
+        # To-do: cancellazione non funziona. Debug.
+        conn            = sqlite3.connect(self.db_path)
+        cursor          = conn.cursor()
+        hashed_password = hashpw(password.encode('utf-8'), gensalt())
+        
+        report = dict()
+        
+        try:
+            # Get user directory.
+            cursor.execute("SELECT directory FROM users WHERE username = ?", (username,))
+            directory = cursor.fetchone()[0]
+            # Delete the user.
+            cursor.execute(
+                """
+                DELETE FROM users WHERE username = ? AND password_hash = ?
+                """,
+                (username, hashed_password)
+            )
+            conn.commit()
+            # Delete user directory.
+            shutil.rmtree(directory)
+            return f"User '{username}' deleted successfully."
+        except sqlite3.IntegrityError:
+            return f"Error: user '{username}' not found or wrong password."
+        finally:
+            conn.close()
 
 
 
---
+++ RegularClient.py
@@ -0,0 +1,112 @@
+from BaseClient import BaseClient
+import rpyc
+
+
+
+class RegularClient(BaseClient):
+    """Client class for regular users."""
+
+    def __init__(self, host="localhost", port=18861):
+        """
+        Initializes the client.
+        Args:
+            host (str): The hostname or IP address of the name server.
+            port (int): The port number of the name server.
+        """
+        super().__init__(host, port)
+
+
+    def display_commands(self):
+        print("""
+        Welcome to sym-DFS Project Client.
+        Commands:
+        login               Log in as a user
+        logout              Log out
+        create-user         Create a new user
+        delete-user         Delete a user
+        exit                Exit
+        show-commands       Show commands
+        """)
+
+
+    def login(self):
+        """Authenticates a regular user."""
+        
+        username    = input("Insert username: ")
+        password    = input("Insert password: ")
+        result      = self.conn.root.authenticate(username, password)
+
+        if result["status"]:
+            self.user_is_logged     = True
+            self.logged_username    = username
+            self.files_dir          = result["directory"]
+        
+        print(result["message"])
+
+
+    def logout(self):
+        """Logs out the current user."""
+        
+        if self.user_is_logged:
+            self.user_is_logged     = False
+            self.logged_username    = None
+            self.files_dir          = None
+            print("Logged out successfully.")
+        else:
+            print("No user is logged in.")
+
+
+    def create_user(self):
+        """Creates a new regular user."""
+        
+        username = input("Insert username: ")
+        password = input("Insert password: ")
+        result = self.conn.root.create_user(username, password, False)
+        print(result)
+
+
+    def delete_user(self):
+        """Deletes a regular user."""
+        
+        # To-do: un utente regolare non deve poter cancellare un utente root.
+        username = input("Insert username: ")
+        password = input("Insert password: ")
+        result = self.conn.root.delete_user(username, password)
+        print(result)
+
+
+    def main_prompt(self):
+        """Main prompt for regular users."""
+        
+        self.connect()              # Connect to the name server.
+        self.display_commands()     # Display the available commands.
+
+        while True:
+            # Get user input.
+            command = input(
+                "({})> ".format(self.logged_username if self.user_is_logged else "non-auth")
+            )
+
+            # Execute the command.
+            match command:
+                case "login":
+                    self.login()
+                case "logout":
+                    self.logout()
+                case "create-user":
+                    self.create_user()
+                case "delete-user":
+                    self.delete_user()
+                case "exit":
+                    print("Exiting...")
+                    break
+                case "show-commands":
+                    self.display_commands()
+                case _:
+                    print("Unknown command. Type 'show-commands' for a list of commands.")
+
+
+
+if __name__ == "__main__":
+    client = RegularClient()
+    client.main_prompt()
